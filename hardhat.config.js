5331bbfe309df32cef8d156ee02b01ac73870a7c
Madhuran Thiagarajah <madhuran.v@gmail.com>
2022-11-11 20:18:07 -0500

    add config for goerli

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,24 @@
+// hardhat.config.js
+require("@nomiclabs/hardhat-ethers");
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config()
 
-/** @type import('hardhat/config').HardhatUserConfig */
+// You need to export an object to set up your config
+// Go to https:// hardhat.org/config/ to learn more. 
+
+const GOERLI_URL = process.env.GOERLI_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.4",
+  networks: {
+    goerli: {
+      url: GOERLI_URL,
+      accounts: [PRIVATE_KEY]
+    }
+  }
 };
